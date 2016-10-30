3969d8cb720526ec412c9c11e0fd40c369213066
Sunshine747 <denisdaniln58@gmail.com>
2016-10-30 19:54:27 +0300

    Добавленны классы для квадрата и прямоугольника

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Администратор on 30.10.2016.
+ */
+public class Rectangle {
+
+    public double a;
+    public double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
